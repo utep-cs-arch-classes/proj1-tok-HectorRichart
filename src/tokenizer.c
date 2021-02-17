d6c5063f4841219077adf12f32628821437e8802
Hector Richart <hrrichartru@miners.utep.edu>
2021-02-17 14:30:27 -0700

    Added cleaner and tokenizer.c file

---
+++ src/tokenizer.c
@@ -0,0 +1,21 @@
+#include "tokenizer.h"
+
+int space_char(char){
+
+}
+
+int non_space_char(char c){
+
+}
+
+char *word_start(char *str){
+
+}
+
+char *word_end(char *str){
+
+}
+
+int count_words(char *str){
+
+}
