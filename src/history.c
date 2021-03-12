272d27fdacbd238daf1e2c48b8e69848b3129f6d
Hector Richart <hrrichartru@miners.utep.edu>
2021-03-12 14:03:25 -0700

    Added history methods

--- src/history.c
+++ src/history.c
@@ -3,8 +3,24 @@
 #include <stdio.h>
 #include "tokenizer.h"
 
-static int dataId = 100;
+//static int dataId = 100;
+/*
+typedef struct s_item{
+  int id;
+  char *str;
+  struct s_item *next;
+  Item;
+}
 
+  typedef struct s_List{
+    struct s_item *root;
+    List;
+
+  }
+
+*/
+
+/*
 List* init_history(){
   List* list = (List*)malloc(sizeof(List));
     list->root = NULL;
@@ -12,16 +28,24 @@ List* init_history(){
 }
 
 void add_history(List *list, char *str){
-  if(list == NULL){
-    return;
+  struct s_item *current = list->root;
+  struct s_item *nextNode = malloc(sizeof(struct s_item));
+  if(current == NULL){
+    list->root = nextNode;
+    nextNode->id=1;
+    nextNode->str = str;
+    nextNode->next = NULL;
   }
-  if(str == NULL){
-    return;
+  else{
+    while(current->next != NULL){
+      current = current->next;
+    }
+    current->next = nextNode;
+    nextNode->id = current->id+1;
+    nextNode->str = str;
+    nextNode->next = NULL;
+ 
   }
-  
-
-
-
   
 }
 
@@ -30,9 +54,32 @@ char *get_history(List *list, int id){
 }
 
 void print_history(List *list){
+  struct s_item *current = list->root;
 
+  if(current == NULL){
+    printf("No items to print");
+  }
+  else{
+    while(current->next != NULL){
+      printf(current);
+      current = current->next;
+    }
+  }
 }
 
 void free_history(List *list){
+  struct s_item *current = list->root;
+  struct s_item *nextNode;
 
+  if(current == NULL){
+    printf("No items to free");
+  }
+  else{
+    while(current->next != NULL){
+      free_tokens(current);
+      current = current->next;
+    }
+
+  }
 }
+*/
